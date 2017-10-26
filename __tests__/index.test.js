f0b18f9a3a848ae0f39de57c684903ff94531d24
Patrick Teich <PatrickTeich@pesite.de>
2017-10-26 18:26:49 +0200

    Update exporting test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -4,5 +4,5 @@ const pureSql = require('../lib/pure-sql.js');
 const index = require('../index.js');
 
 test('should export pureSql properly', function() {
-    expect(index.parseTemplates).toEqual(pureSql.parseTemplates);
+    expect(index).toEqual(pureSql);
 });
